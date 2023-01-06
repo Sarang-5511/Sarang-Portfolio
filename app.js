73f07d6786f80bd1013932b2f166b37fc2a36272
Sarang Bang <bangsarang7@gmail.com>
2023-01-06 21:31:13 +0530

    4

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "#F5F5F5",
+      "background_color": "#FFFFFF",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
